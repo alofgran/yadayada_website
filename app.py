af2a0b466c170e221fbfb428b896b23f7a7e92c2
ryan-egbert <ryanegbert@aggiemail.usu.edu>
2020-06-29 16:31:27 -0600

    Added database

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(100), unique=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.route("/")
 def main():
